635433747dcd4d3b357ff3166280a3c6f3e06864
Thibault de Boissiere <thibault.deboissiere@seeingmachines.com>
2017-01-23 10:29:17 +1100

    Update pandas_tips.py

--- cookbook/python/pandas_tips.py
+++ cookbook/python/pandas_tips.py
@@ -3,3 +3,12 @@ Read csv file without header
 '''
 df = pd.read_csv(eval_file, delimiter=" ", header=None)
 df.columns = ["img_path", "dset_idx"]
+
+
+'''
+Simple date indexing
+'''
+
+# assuming a time column ['2016-01-07', '2016-01-08' ..]
+dft = pd.to_datetime(df["time"])
+idx = dft < "2016-01-10" # will give correct selection
