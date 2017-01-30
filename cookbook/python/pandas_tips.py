82e70b0fa16d08fcb91cf9280ac5ca5a883df854
Thibault de Boissiere <thibault.deboissiere@seeingmachines.com>
2017-01-30 13:28:48 +1100

    Update pandas_tips.py

--- cookbook/python/pandas_tips.py
+++ cookbook/python/pandas_tips.py
@@ -18,3 +18,10 @@ Adding columns
 '''
 
 #faster to concatenate two dataframes than adding a column as in df["C"] = blabla
+
+
+'''
+Concatenating dataframe
+'''
+
+# Make sure indices are compatible, otherwise unexpected results
