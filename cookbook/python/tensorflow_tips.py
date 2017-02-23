3f6430ccd1fbcb9716629efe3488ae8e510bd2c8
Thibault de Boissiere <thibault.deboissiere@seeingmachines.com>
2017-02-23 18:06:02 +1100

    Update tensorflow_tips.py

--- cookbook/python/tensorflow_tips.py
+++ cookbook/python/tensorflow_tips.py
@@ -2,15 +2,23 @@
 
 '''
 Variable initialization:
+'''
 
-sess = tf.Sesstion()
+sess = tf.Session()
 x = tf.Variable(initial_value=1.0, trainable=False)
 sess.run(x)
 
 # will throw an error because x is not initialized
 
-sess = tf.Sesstion()
+sess = tf.Session()
 x = tf.random_normal(10)
 sess.run(x)
 
 # will not throw an error because x does not need to be initialized
+
+
+'''
+Variables defined within a scope
+'''
+
+tf.get_collection(tf.GraphKeys.VARIABLES, scope=scope.name)
