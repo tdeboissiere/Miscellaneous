7a5bd21c4ce68f785a80510d6fdca43ef95620f6
Thibault de Boissiere <thibault.deboissiere@seeingmachines.com>
2017-05-02 15:06:09 +1000

    Update python_tips.py

--- cookbook/python/python_tips.py
+++ cookbook/python/python_tips.py
@@ -445,3 +445,16 @@ for f in arr_extracted_files:
     ll.append(out)
 plt.hist(ll, bins=100)
 plt.show()
+
+"""
+Break out of nested loops
+"""
+class Found(Exception): pass
+try:
+    for i in range(100):
+        for j in range(1000):
+            for k in range(10000):
+               if i + j + k == 777:
+                  raise Found
+except Found:
+    print i, j, k 
