cdb54cd33e5601578656afd9487ee0a64a8a998a
Thibault de Boissiere <thibault.deboissiere@seeingmachines.com>
2017-07-20 16:37:30 +1000

    Update matplotlib_tips.py

--- cookbook/python/matplotlib_tips.py
+++ cookbook/python/matplotlib_tips.py
@@ -380,4 +380,76 @@ Axis equal aspect + automatic data boundaries
 """
 ax.set_aspect('equal', 'box')
 
+"""
+Control 3D axis orientation
+"""
+ax.view_init(elev=-64., azim=97)
 
+"""
+Circles in 3D
+"""
+
+def rotation_matrix(v1,v2):
+    """
+    Calculates the rotation matrix that changes v1 into v2.
+    """
+    v1 /= np.linalg.norm(v1)
+    v2 /= np.linalg.norm(v2)
+
+    cos_angle = np.dot(v1,v2)
+    d = np.cross(v1,v2)
+    sin_angle = np.linalg.norm(d)
+
+    if sin_angle == 0:
+        M = np.identity(3) if cos_angle > 0. else -np.identity(3)
+    else:
+        d /= sin_angle
+
+        eye = np.eye(3)
+        ddt = np.outer(d, d)
+        skew = np.array([[0, d[2], -d[1]],
+                         [-d[2], 0, d[0]],
+                         [d[1], -d[0], 0]], dtype=np.float64)
+
+        M = ddt + cos_angle * (eye - ddt) + sin_angle * skew
+
+    return M
+
+
+def pathpatch_2d_to_3d(pathpatch, z=0, normal='z'):
+    """
+    Transforms a 2D Patch to a 3D patch using the given normal vector.
+
+    The patch is projected into they XY plane, rotated about the origin
+    and finally translated by z.
+    """
+    if type(normal) is str:  # Translate strings to normal vectors
+        index = "xyz".index(normal)
+        normal = np.roll((1,0,0), index)
+
+    path = pathpatch.get_path()  # Get the path and the associated transform
+    trans = pathpatch.get_patch_transform()
+
+    path = trans.transform_path(path)  # Apply the transform
+
+    pathpatch.__class__ = art3d.PathPatch3D  # Change the class
+    pathpatch._code3d = path.codes  # Copy the codes
+    pathpatch._facecolor3d = pathpatch.get_facecolor  # Get the face color
+
+    verts = path.vertices  # Get the vertices in 2D
+
+    M = rotation_matrix(normal,(0, 0, 1))  # Get the rotation matrix
+
+    pathpatch._segment3d = np.array([np.dot(M, (x, y, 0)) + (0, 0, z) for x, y in verts])
+
+
+def pathpatch_translate(pathpatch, delta):
+    """
+    Translates the 3D pathpatch by the amount delta.
+    """
+    pathpatch._segment3d += delta
+    
+c = Circle((0,0), v.pupil_radius, edgecolor="C0", facecolor='C0', alpha=.2)
+ax.add_patch(c)
+pathpatch_2d_to_3d(c, z=0, normal=xxx)
+pathpatch_translate(c, np.array([x,y,z]))
